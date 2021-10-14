d0ee7382c587f2cd5e60f64e9b3aa6e209a9c318
Taseeeer <syedtaseeershah@gmail.com>
2021-10-14 14:17:12 +0500

    the start

---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,10 @@
+import Directory from "../directory/Directory";
+import "../homepage/homepage.styles.scss";
+
+export default function Homepage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
---
+++ src/components/menu-item/MenuItem.js
@@ -0,0 +1,17 @@
+import "./menu-item.styles.scss";
+
+export default function MenuItem({ title, imageUrl, size }) {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl ? imageUrl : ""})`,
+      }}
+      className={`${size} menu-item`}
+    >
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+}
